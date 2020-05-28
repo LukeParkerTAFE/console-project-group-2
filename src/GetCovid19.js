62489b2f31800aef7d11456e0b54a65a14bd5ee3
Sharmin Alam <sharmin.alam4@studytafensw.edu.au>
2020-05-28 16:30:14 +1000

    latest work for covid-19 app

--- src/GetCovid19.js
+++ src/GetCovid19.js
@@ -20,13 +20,14 @@ function askQuestion(question) {
 
 
 async function Program() {
-    console.log("Welcome to the Covid-19 status app!");
+    console.log("Welcome to the Covid-19 Status APP!");
     while (true) {
         console.log("Select from the below options:")
         console.log("[1] Confirmed Cases");
         console.log("[2] Recovered Cases");
         console.log("[3] Death Count");
-        console.log("[4] Exit");
+        console.log("[4] Countries Total Stats");
+        console.log("[5] Exit");
         let userInput = await askQuestion("Select an option from above: ");
         console.log()
         switch (userInput) {
@@ -35,21 +36,51 @@ async function Program() {
                 let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed`);
                 let dateResults = response.data
                 let mostRecentResult = dateResults[dateResults.length -1];
-                console.log(mostRecentResult)
+               // console.log(mostRecentResult)
+               console.log(`Confirmed Cases For ${country}:`);
+            
+                console.log(mostRecentResult.Confirmed);
                 // let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed/date/2020-03-21T13:13:30Z`);
                 // let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed/date/2020-03-21T13:13:30Z`)
-                // console.log(response.data);
+                 //console.table(dateResults,["Date","Recovered","Confirmed","Deaths"],dateResults.shift());
                 break;
-            // case "2":
-            //     let country = await askQuestion("Which country do you want the stats for Covid-19: ");
-            //     let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/recovered`);
-            //     console.log(response);
-            //     break;
-            // case "3":
-            //     let country = await askQuestion("Which country do you want the stats for Covid-19: ");
-            //     let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/deaths`);
-            //     console.log(response);
-            //     break;
+            case "2":
+                let country2 = await askQuestion("Which country do you want the stats for Covid-19: ");
+                let response2 = await axios.get(`https://api.covid19api.com/live/country/${country2}/status/recovered`);
+                let dateResults2 = response2.data
+                let mostRecentResult2 = dateResults2[dateResults2.length -1];
+               // console.log(mostRecentResult)
+              console.log(`Recovered Cases For ${country2}:`);
+                console.log(mostRecentResult2.Recovered)
+              //  console.log(response2);
+                break;
+            case "3":
+                let country3 = await askQuestion("Which country do you want the stats for Covid-19: ");
+                let response3 = await axios.get(`https://api.covid19api.com/live/country/${country3}/status/deaths`);
+                
+                let dateResults3 = response3.data
+                let mostRecentResult3 = dateResults3[dateResults3.length -1];
+               // console.log(mostRecentResult)
+                console.log(`Death Count For ${country3}:`);
+                console.log(mostRecentResult3.Deaths)
+                break;
+            case "4":
+                let country4 = await askQuestion("Which country do you want the stats for Covid-19: ");
+                let response4 = await axios.get(`https://api.covid19api.com/live/country/${country4}/status/confirmed`);
+                let dateResults4 = response4.data
+                let mostRecentResult4 = dateResults4[dateResults4.length -1];
+               // console.log(mostRecentResult)
+              // console.log("confirmd case");
+                //console.log(mostRecentResult.Confirmed)
+                // let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed/date/2020-03-21T13:13:30Z`);
+                // let response = await axios.get(`https://api.covid19api.com/live/country/${country}/status/confirmed/date/2020-03-21T13:13:30Z`)
+                console.log(`${country4} Stats`);
+                console.table(dateResults4,["Date","Confirmed","Recovered","Deaths"]);
+                break;
+            case "5":
+                console.log("Thank you for using the Covid-19 Status APP.");
+                console.log();
+                process.exit(0);
             default:
                 console.log();
                 console.log("ERROR: Input Invalid. Please entere a number between 1 to 4.");
